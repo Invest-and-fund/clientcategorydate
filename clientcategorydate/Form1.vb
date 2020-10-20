e5d44216a93347a89359a97a2d7d189bf7c06abe
Ian Stabler <Ian@investandfund.com>
2020-10-20 17:32:08 +0100

    Add project files.

---
+++ clientcategorydate/Form1.vb
@@ -0,0 +1,21 @@
+﻿Imports System.Globalization
+Imports System.Configuration
+Imports System.Collections.Specialized
+Imports System.IO
+Imports System.Data
+Imports System.Data.SqlClient
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim strConn As String
+        Dim MyConn As FirebirdSql.Data.FirebirdClient.FbConnection
+        Dim Cmd As FirebirdSql.Data.FirebirdClient.FbCommand
+        Dim Reader As FirebirdSql.Data.FirebirdClient.FbDataReader
+
+        Dim Adaptor As FirebirdSql.Data.FirebirdClient.FbDataAdapter
+
+        strConn = ConfigurationManager.ConnectionStrings("FBConnectionString").ConnectionString
+        MyConn = New FirebirdSql.Data.FirebirdClient.FbConnection(strConn)
+        MyConn.Open()
+    End Sub
+End Class
